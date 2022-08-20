671992e92d4d6167f4e05ca3ba0e0b38e93632f5
David Hybner <dah1998@yahoo.com>
2022-08-19 23:47:32 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    Question: "What is your Project called?"
+},
+{
+    Question: "What was your motivation?"
+},
+{
+    Question: "How do you install your project?"
+},
+{
+    Question: "Usage?"
+},
+{
+    Question: "Do you have any license?"
+},
+{
+    Question: "Contributing?"
+},
+{
+    Question: "Tests"
+},
+{
+    Question: "Any additonal questions for future development put here"
+},
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
